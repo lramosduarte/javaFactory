251080c2003efd0d8ca4f16fbff040636789f7f4
Leonardo Ramos Duarte <lramosduarte@gmail.com>
2019-08-03 16:15:12 -0300

    Add tests using lombok

--- src/test/java/com/lramosduarte/fake/FakeDataGeneratorPrivateAtributesAndMethodsGetTest.java
+++ src/test/java/com/lramosduarte/fake/FakeDataGeneratorPrivateAtributesAndMethodsGetTest.java
@@ -1,5 +1,6 @@
 package com.lramosduarte.fake;
 
+import com.lramosduarte.classutils.ClassUsingLombok;
 import com.lramosduarte.classutils.ClassWithPrivateAttributesAndGetMethods;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,25 @@ public class FakeDataGeneratorPrivateAtributesAndMethodsGetTest {
     @Test
     public void testGenerateValuesObjectesPrivatesAttributesUseGetMethod_returnValueGenerated() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
         ClassWithPrivateAttributesAndGetMethods instance = new FakeDataGenerator().make(ClassWithPrivateAttributesAndGetMethods.class);
+        Assertions.assertNotNull(instance.getAtrString());
+    }
+
+    @Test
+    public void testClassUsingLombokGenerateValuesObjectesPrivatesAttributes_returnInstanceWithSuccess() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
+        ClassUsingLombok instance = new FakeDataGenerator().make(ClassUsingLombok.class);
+        Assertions.assertNotNull(instance);
+    }
+
+    @Test
+    public void testClassUsingLombokNotGenerateValueToPrivateAttributeAndUseGetMethod_returnNull() {
+        ClassUsingLombok instance = new ClassUsingLombok();
+        Assertions.assertNull(instance.getAtrString());
+    }
+
+
+    @Test
+    public void testClassUsingLombokGenerateValuesObjectesPrivatesAttributesUseGetMethod_returnValueGenerated() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
+        ClassUsingLombok instance = new FakeDataGenerator().make(ClassUsingLombok.class);
         Assertions.assertNotNull(instance.getAtrString());
     }
 
